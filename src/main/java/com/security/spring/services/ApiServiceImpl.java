0d5e5cf5055de69f1fd97eabb51da1b1eb874f49
mirannaalina <v_mxa@yahoo.com>
2021-03-18 16:42:12 +0200

    externalized api

--- src/main/java/com/security/spring/services/ApiServiceImpl.java
+++ src/main/java/com/security/spring/services/ApiServiceImpl.java
@@ -3,8 +3,11 @@ package com.security.spring.services;
 
 import com.security.spring.model.User;
 import com.security.spring.model.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.UriComponentsContributor;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -13,15 +16,21 @@ public class ApiServiceImpl implements ApiService{
 
     private RestTemplate restTemplate;
 
-    public ApiServiceImpl(RestTemplate restTemplate){
+    private final String api_url;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url){
         this.restTemplate=restTemplate;
+        this.api_url =api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit){
 
 
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(api_url).queryParam("limit",limit);
+
+
+        UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 
         return userData.getData();
     }
